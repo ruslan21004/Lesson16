0173f43ce857627ffc78f6ba929388c632f6348b
Ruslan Savchenko <90481548+ruslan21004@users.noreply.github.com>
2022-09-19 08:59:22 +0300

    firstcommit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Homework16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Topic topic = new Topic();
+            Console.WriteLine(topic.topic("Тема голосования " + " "));
+
+            Voting voting = new Voting();
+            voting.voting();
+        }
+    }
+
+    class Topic
+    {
+        public string topic(string message)
+        {
+            Console.WriteLine("Напишите тему голосования");
+            string voteTopic = Console.ReadLine();
+            return message + voteTopic;
+        }
+    }
+
+    class Voting
+    {
+        public void voting()
+        {
+            Console.WriteLine("\nСделайте выбор, напишите да или нет");
+            List<string> votingList = new List<string> { };
+            votingList.Add(Console.ReadLine());
+            if (votingList.Contains("Да"))
+            {
+                Console.WriteLine("Голосование окончено, решение сделано");
+            }
+            else
+            {
+                Console.WriteLine("Голосование окончено, решение не сделано");
+            }
+        }
+    }
+}
